0a518229a47e01653aa73be506c11e27cadf65f0
Gašper Petelin <gapi.petelin@gmail.com>
2026-08-25 17:09:09 +0200

    Add sleep intervals between benchmark submissions for improved execution flow

--- scripts/submit_benchmark.sh
+++ scripts/submit_benchmark.sh
@@ -8,25 +8,25 @@ COMMON2=(--reservation=e7 --cpus-per-task=24 --mem-per-cpu=8G --array=0-10 --tim
 # CPU baselines
 sbatch "${COMMONHC[@]}" --time=72:00:00 --time-min=24:00:00 --mem-per-cpu=6G scripts/run_benchmark2.slurm -c HIVECOTEV2
 sbatch "${COMMONHC[@]}" --time=72:00:00 --time-min=24:00:00 --mem-per-cpu=6G --reservation=e7 scripts/run_benchmark2.slurm -c HIVECOTEV2
-
+sleep 2
 sbatch "${COMMONHC[@]}" --time=72:00:00 --mem-per-cpu=6G scripts/run_benchmark2.slurm -c HIVECOTEV2
 sbatch "${COMMONHC[@]}" --time=72:00:00 --mem-per-cpu=6G --reservation=e7 scripts/run_benchmark2.slurm -c HIVECOTEV2
-
+sleep 2
 sbatch "${COMMONHC[@]}" --time=72:00:00 --mem-per-cpu=12G scripts/run_benchmark2.slurm -c HIVECOTEV2
 sbatch "${COMMONHC[@]}" --time=72:00:00 --mem-per-cpu=12G --reservation=e7 scripts/run_benchmark2.slurm -c HIVECOTEV2
-
+sleep 2
 sbatch "${COMMON0[@]}" --time=04:00:00 scripts/run_benchmark2.slurm -c ROCKET
 sbatch "${COMMON0[@]}" --time=04:00:00 scripts/run_benchmark2.slurm -c MultiRocketHydra
 sbatch "${COMMON0[@]}" --time=04:00:00 scripts/run_benchmark2.slurm -c Catch22
-
+sleep 2
 sbatch "${COMMON1[@]}" --time=04:00:00 scripts/run_benchmark2.slurm -c ROCKET
 sbatch "${COMMON1[@]}" --time=04:00:00 scripts/run_benchmark2.slurm -c MultiRocketHydra
 sbatch "${COMMON1[@]}" --time=04:00:00 scripts/run_benchmark2.slurm -c Catch22
-
+sleep 2
 sbatch "${COMMON2[@]}" --time=08:00:00 --gres=gpu:1 scripts/run_benchmark2.slurm -c TSCGlueEnhancedV4-Low-Accuracy-GPU
 sbatch "${COMMON2[@]}" --time=08:00:00 --gres=gpu:1 scripts/run_benchmark2.slurm -c TSCGlueEnhancedV4-Medium-Accuracy-GPU
 sbatch "${COMMON2[@]}" --time=08:00:00 --gres=gpu:1 scripts/run_benchmark2.slurm -c TSCGlueEnhancedV4-High-Accuracy-GPU
-
+sleep 2
 sbatch "${COMMON2[@]}" --time=08:00:00 --gres=gpu:1 scripts/run_benchmark2.slurm -c TSCGlueEnhancedV4-Low-LogLoss-GPU
 sbatch "${COMMON2[@]}" --time=08:00:00 --gres=gpu:1 scripts/run_benchmark2.slurm -c TSCGlueEnhancedV4-Medium-LogLoss-GPU
 sbatch "${COMMON2[@]}" --time=08:00:00 --gres=gpu:1 scripts/run_benchmark2.slurm -c TSCGlueEnhancedV4-High-LogLoss-GPU
